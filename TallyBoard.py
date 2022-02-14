b321bc39bfca354c687c31dfb2e645bc060cf5e9
JAndrew13 <Jbbrunner10@gmail.com>
2022-02-13 18:58:22 -0800

    create blank Tkinter GUI

---
+++ TallyBoard.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Button, Label, Scrollbar, Listbox, StringVar, Entry, N, W,  E, S, END
+from tkinter import ttk
+from tkinter import messagebox
+
+window = Tk()
+
+
+
+window.mainloop()
