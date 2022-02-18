9fe2701b2e83f70026acd56ca6ef91c906d756fe
JAndrew13 <Jbbrunner10@gmail.com>
2022-02-17 22:11:19 -0800

    continue GUI styling

--- GUI.py
+++ GUI.py
@@ -42,27 +42,15 @@ class StartPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         tk.Tk.configure(self, background="Orange")
 
-        spacer1 = tk.Label(self, text="", )
-        spacer1.grid(row=0, column=0)
-
         employees_label = ttk.Label(self, background="Orange", text="Available Workers", font=FONT_BODY)
-        employees_label.grid(row=1, column=0)
+        employees_label.place(x=25 , y=100)
 
         self.WorkerDisplay(data)
 
-        # INSERT DIVIDE LINE
-
-        spacer2 = tk.Label(self, text="")
-        spacer2.grid(row=0, column=2,)
-        spacer3 = tk.Label(self, text="")
-        spacer3.grid(row=1, column=2, )
-
-        # INSERT DIVIDE LINE
-
         editEmp_btn = ttk.Button(self, text="Edit Employees",command=lambda: controller.show_frame(EditEmployees))
-        editEmp_btn.grid(row=4, column=0)
+        editEmp_btn.place(x=25, y=550)
         viewTally_btn = ttk.Button(self,text="View Tally", command=lambda: controller.show_frame(Stats))
-        viewTally_btn.grid(row=5, column=0)
+        viewTally_btn.place(x=300, y=550)
 
         amount_5_btn = ttk.Button(self, text="5")
         amount_5_btn.place(x= 225, y=70)
@@ -81,24 +69,51 @@ class StartPage(tk.Frame):
         amount_800_btn = ttk.Button(self, text="800")
         amount_800_btn.place(x= 310, y=169)
 
+        custom_label = ttk.Label(text="Cust. Amount:", font=("Veranda", 8, "bold"), background="orange")
+        custom_label.place(x=225, y=200)
+        custom_entry = ttk.Entry(width=12)
+        custom_entry.place(x=310, y=202)
+
+        total_label = ttk.Label(text="Current Total:", font=("Veranda", 8, "bold"), background="orange")
+        total_label.place(x=225, y=232)
+        total_disp_label = ttk.Label(text="XXXX", font=("Veranda", 10, 'bold'), background="orange")
+        total_disp_label.place(x=310, y=230)
+
+        reset_button = ttk.Button(text="Reset Value", width= 25)
+        reset_button.place(x=225, y=265)
         add_work_button = ttk.Button(self, text="Add Work")
-        add_work_button.place(x= 225, y=220)
+        add_work_button.place(x= 225, y=300)
         sub_work_button = ttk.Button(self, text="Sub Work")
-        sub_work_button.place(x= 310, y=220)
-    def WorkerDisplay(self, data):
-        lb_workers = tk.Listbox(self, bd=4, font="Veranda", selectbackground="orange")
-        lb_workers.grid(row=3, column=0, padx=20, pady=20 )
-        for worker in data:
+        sub_work_button.place(x= 310, y=300)
+
+        # smiley = ttk.Label(text="😁😃😅😓😣😠", font=("Veranda", 18, "bold"), background="orange")
+        # smiley.place(x=50, y=400)
+
+# ================  TEMP DISPLAY  ========================
+#     def WorkerTally(self):
+        worker_name = "john"
+        worker_tally = "2082"
+        worker_mood = "😃"
+
+        slot1 = ttk.Label(text=f"{worker_name} - ${worker_tally}",
+                          font=("Veranda", 15, "bold"),
+                          background='white')
+        slot1.place(x=50, y=350)
+        slot2 = ttk.Label(text=worker_mood, font=("Veranda", 18, "bold"), background="orange")
+        slot2.place(x=15, y=345)
+
+
+    def WorkerDisplay(self, worker_names):
+        lb_workers = tk.Listbox(self, bd=3, font="Veranda", selectbackground="orange", height=13)
+        lb_workers.place(x=15, y=70)
+        for worker in worker_names:
             lb_workers.insert(END, worker)
-
-
-
 
 class EditEmployees(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = ttk.Label(self, text="Edit Employees Page", font=FONT_BODY)
-        label.grid(row=0, column=1 )
+        label.grid(row=0, column=1)
 
         return_btn = ttk.Button(self, text='Return to Main', width=25, command=lambda: controller.show_frame(StartPage))
         return_btn.grid(row=10, column=1)
